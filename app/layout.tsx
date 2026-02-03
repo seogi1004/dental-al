fc17ac05ddb5dc9f76cd384afe7de3ddc25e5064
alvin <alvin@jennifersoft.com>
2026-02-03 12:23:52 +0900

    chore: add standard mobile-web-app-capable meta tag to silence deprecation warning

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,9 @@ export const metadata: Metadata = {
   formatDetection: {
     telephone: false,
   },
+  other: {
+    "mobile-web-app-capable": "yes",
+  },
 };
 
 export const viewport: Viewport = {
